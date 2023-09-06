a3ab7c93cfbf708d9679466122227b49e0a9501f
Your Name <e>
2023-09-06 12:32:16 +0300

    Podolsky 06-09-23

---
+++ 339/index.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    // Код, который нужно выполнить при клике на кнопку
+    let elem1 = document.querySelector('#elem1');
+    let elem2 = document.querySelector('#elem2');
+    let elem3 = document.querySelector('#elem3');
+
+    alert(elem1.innerText);
+    alert(elem2.innerText);
+    alert(elem3.innerText);
+}
+
