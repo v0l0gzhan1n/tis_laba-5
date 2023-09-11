6087bd78b40a7f3382cbd100a842a8cbdf1b914d
Stepan Fedulov <safifed23532@gmail.com>
2023-09-11 12:17:43 +0300

    Podolsky 11-09-23

---
+++ Введение в DOM/341/index.js
@@ -0,0 +1,17 @@
+   // Код, который нужно выполнить при клике на кнопку
+    let elem1 = document.querySelector('#button1');
+    let elem2 = document.querySelector('#button2');
+    let elem3 = document.querySelector('#button3');
+
+    button1.addEventListener('click', function() {
+    alert(1);
+    });
+
+    button2.addEventListener('click', function() {
+    alert(2);
+    });
+
+    button3.addEventListener('click', function() {
+    alert(3);
+});
+
